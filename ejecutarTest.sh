234db9088cb53a04e4d9d4ac2c0b1de45041f097
fuster <626901@unizar.es>
2020-05-26 12:08:13 +0200

    Commit final

--- ejecutarTest.sh
+++ ejecutarTest.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
+#$1 = numero de ejecuciones
+#$2 = longitud del vector
+#$3 = nombre del archivo de salida
 for (( i=0;i<$1;i++ ))
 do
- java -jar ./Practica5.jar >> test.csv
+ java -jar ./Practica5.jar 1 $2 300 >> $3.csv
 done
