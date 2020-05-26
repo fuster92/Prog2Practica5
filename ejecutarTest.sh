526e3df5e7b9c8568f8e29a89b268fa1a7f425d9
fuster <626901@unizar.es>
2020-05-26 12:08:59 +0200

    Final del todo

--- ejecutarTest.sh
+++ ejecutarTest.sh
@@ -2,7 +2,7 @@
 #$1 = numero de ejecuciones
 #$2 = longitud del vector
 #$3 = nombre del archivo de salida
-for (( i=0;i<$1;i++ ))
+for (( i=0; i < $1; i++ ))
 do
- java -jar ./Practica5.jar 1 $2 300 >> $3.csv
+ java -jar ./Practica5.jar 1 "$2" 300 >> "$3".csv
 done
